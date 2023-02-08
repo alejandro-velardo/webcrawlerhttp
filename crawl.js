89f1b32c8351bd86403c251fbafebabd401fe995
Alejandro Velardo <a.velardo.lucero@gmail.com>
2023-02-08 09:31:59 +0100

    getURLsFromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,5 +1,31 @@
-const { isGeneratorFunction } = require("util/types")
+const { link } = require('fs')
+const { JSDOM } =  require('jsdom')
 
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for (let link of linkElements) {
+        if (link.href.slice(0,1) === "/") {
+            //relative
+            try {
+            const urlObj = new URL (`${baseURL}${link.href}`)
+            urls.push(urlObj.href)
+            } catch (err) {
+                console.log(`error with relative url: ${err.message}`)
+            }
+        } else {
+            //absolute
+            try {
+                const urlObj = new URL(link.href)
+                urls.push(urlObj.href)
+            } catch (err) {
+                console.log(`error with absolute url: ${err.message}`)
+            }
+        }
+    }
+    return  urls
+}
 function normalizeURL(urlString) {
     const urlObj = new URL(urlString)
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`
@@ -10,5 +36,6 @@ function normalizeURL(urlString) {
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
